45654d3946b734ed3e0133f02a8bb4e7a4b201d8
Raekwon Williams <61262708+Raekwill15@users.noreply.github.com>
2021-01-14 14:02:00 -0500

    Update and rename citiesinfo.jsx to Zips.jsx

---
+++ citysearch/src/Components/Zips.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class Zips extends Component {
+    render() {
+        return (
+            <div>
+                <p>{this.props.val}</p>
+            </div>
+        )
+    }
+}
+
+export default Zips
